41299523021b9fc6aa66ca59a5afee564b6ec205
dlesmesl <dlesmesl@unbosque.edu.co>
2021-02-27 16:45:32 +0100

    plugin_v1

--- napari_hdf5_io/_reader.py
+++ napari_hdf5_io/_reader.py
@@ -1,78 +1,35 @@
-"""
-This module is an example of a barebones numpy reader plugin for napari.
-
-It implements the ``napari_get_reader`` hook specification, (to create
-a reader plugin) but your plugin may choose to implement any of the hook
-specifications offered by napari.
-see: https://napari.org/docs/dev/plugins/hook_specifications.html
-
-Replace code below accordingly.  For complete documentation see:
-https://napari.org/docs/dev/plugins/for_plugin_developers.html
-"""
-import numpy as np
 from napari_plugin_engine import napari_hook_implementation
+import numpy as np
+import sparse
+import h5py
 
 
-@napari_hook_implementation
-def napari_get_reader(path):
-    """A basic implementation of the napari_get_reader hook specification.
-
-    Parameters
-    ----------
-    path : str or list of str
-        Path to file, or list of paths.
-
-    Returns
-    -------
-    function or None
-        If the path is a recognized format, return a function that accepts the
-        same path or list of paths, and returns a list of layer data tuples.
-    """
-    if isinstance(path, list):
-        # reader plugins may be handed single path, or a list of paths.
-        # if it is a list, it is assumed to be an image stack...
-        # so we are only going to look at the first file.
-        path = path[0]
-
-    # if we know we cannot read the file, we immediately return None.
-    if not path.endswith(".npy"):
+@napari_hook_implementation(specname='napari_get_reader')
+def h5_to_napari(path):
+    if isinstance(path, str) and path.endswith('.h5'):
+        return read_layer_h5
+    else:
         return None
 
-    # otherwise we return the *function* that can read ``path``.
-    return reader_function
+
+def read_layer_h5(path):
+    output_list = []
+    with h5py.File(path, 'r') as hdf:
+        for layer_type in hdf:  # iterate over layer types
+            for ith_layer in hdf[layer_type]:  # iterate over each layer
+                tmp_layer = hdf[layer_type][ith_layer]
+                layer_data = np.array(tmp_layer)
+                layer_meta = {key: val for key, val in tmp_layer.attrs.items()}
+                if layer_type == 'labels':
+                    original_shape = layer_meta.pop(
+                        'shape')  # delete tmp shape attribute
+                    layer_data = reconstruct_layer(layer_data, original_shape)
+                output_list.append((layer_data, layer_meta, layer_type))
+    return output_list
 
 
-def reader_function(path):
-    """Take a path or list of paths and return a list of LayerData tuples.
-
-    Readers are expected to return data as a list of tuples, where each tuple
-    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
-    both optional.
-
-    Parameters
-    ----------
-    path : str or list of str
-        Path to file, or list of paths.
-
-    Returns
-    -------
-    layer_data : list of tuples
-        A list of LayerData tuples where each tuple in the list contains
-        (data, metadata, layer_type), where data is a numpy array, metadata is
-        a dict of keyword arguments for the corresponding viewer.add_* method
-        in napari, and layer_type is a lower-case string naming the type of layer.
-        Both "meta", and "layer_type" are optional. napari will default to
-        layer_type=="image" if not provided
-    """
-    # handle both a string and a list of strings
-    paths = [path] if isinstance(path, str) else path
-    # load all files into array
-    arrays = [np.load(_path) for _path in paths]
-    # stack arrays into single array
-    data = np.squeeze(np.stack(arrays))
-
-    # optional kwargs for the corresponding viewer.add_* method
-    add_kwargs = {}
-
-    layer_type = "image"  # optional, default is "image"
-    return [(data, add_kwargs, layer_type)]
+def reconstruct_layer(layer_array, shape):
+    coords = layer_array[:-1]
+    values = layer_array[-1]
+    tmp_sparse = sparse.COO(coords=coords, data=values, shape=shape)
+    return tmp_sparse.todense()
--- napari_hdf5_io/_writer.py
+++ napari_hdf5_io/_writer.py
@@ -1,20 +1,35 @@
-"""
-This module is an example of a barebones writer plugin for napari
-
-It implements the ``napari_get_writer`` and ``napari_write_image`` hook specifications.
-see: https://napari.org/docs/dev/plugins/hook_specifications.html
-
-Replace code below according to your needs
-"""
-
 from napari_plugin_engine import napari_hook_implementation
+import numpy as np
+import sparse
+import h5py
 
 
-@napari_hook_implementation
-def napari_get_writer():
-    pass
+@napari_hook_implementation(specname='napari_get_writer')
+def project_to_h5(path, layer_types):
+    if isinstance(path, str) and path.endswith('.h5'):
+        return write_layers_h5
+    else:
+        return None
 
 
-@napari_hook_implementation
-def napari_write_image():
-    pass
+def write_layers_h5(path, layer_data):
+    with h5py.File(path, 'w') as hdf:
+        for data, meta, layer_type in layer_data:
+            layer_name = meta['name']
+            if layer_type not in hdf.keys():  # check if the layer_type group already exists
+                hdf.create_group(layer_type)
+            if layer_type == 'labels':
+                # make compression in the same data variable
+                meta['shape'], data = compress_layer(data)
+            hdf[layer_type].create_dataset(layer_name, data=data)
+            for key, val in meta.items():  # to add all metadata as attributes of each layer
+                hdf[layer_type][layer_name].attrs[key] = val
+    return path
+
+
+def compress_layer(layer_array):
+    initial_shape = layer_array.shape
+    tmp_coo = sparse.COO(layer_array)
+    compressed_array = np.append(tmp_coo.coords, np.array(
+        [tmp_coo.data]), axis=0)  # join coords and data in single array
+    return initial_shape, compressed_array
