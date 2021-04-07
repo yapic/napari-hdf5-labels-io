2029387e6e1af5a1d818516ea2420973da7f1ecb
Pranjal Dhole <pranjal.dhole@dzne.de>
2021-04-07 11:50:57 +0200

    bug fix in testing

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    python_requires='<=3.8',
+    python_requires='<3.9',
     install_requires=requirements,
     use_scm_version={"local_scheme": local_scheme},
     setup_requires=['setuptools_scm'],
